6f813d8c8d7dec5e82b9974a0325493ee064b38e
Justin Butwell <jbutwell@hotmail.com>
2016-12-05 16:08:30 +0000

    Adding comments for each step

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first e print a list of students
 puts "The sytudents of Villians Academy"
 puts "______________"
 puts "Dr. Hannibal Lector"
@@ -11,7 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+# finally, we print the total number of students
 print "Overall, we have "
+# it's important that pring() doesn't add new line characters
 print 11
 puts " great students"
