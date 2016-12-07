ba6d51f0d970a145db3d049ad8626389dbb20583
Justin Butwell <jbutwell@hotmail.com>
2016-12-07 10:35:01 +0000

    Refactoring more code to include an array and iteration

--- directory.rb
+++ directory.rb
@@ -1,18 +1,22 @@
-student_count = 11
-
 # first e print a list of students
-puts "The sytudents of Villians Academy"
+students = [
+  "Dr. Hannibal Lector",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+# and then print them
+puts "The students of Villians Academy"
 puts "______________"
-puts "Dr. Hannibal Lector"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts student 
+end
 # finally, we print the total number of students
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
