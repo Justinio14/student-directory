8c969625fd2653e6e449f4d10e4b25e2621051cf
Justin Butwell <jbutwell@hotmail.com>
2016-12-08 14:52:09 +0000

    Exercise 8.2 print names that start with a specific letter

---
+++ ex8.2.rb
@@ -0,0 +1,56 @@
+# first e print a list of students
+students = [
+  {name: "Dr. Hannibal Lector", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+def print_header
+  puts "The students of Villians Academy"
+  puts "______________"
+end
+
+def print(students, letter)
+
+  # 2. by adding a second argument will return on students whos name begin with that letter
+  students.select! { |student| student[:name][0] == letter}
+
+  students.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+# nothing will happen until we call the methods
+students = input_students
+print_header
+print(students, "b")
+print_footer(students)
